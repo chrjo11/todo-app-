5da01d9dbe35ed8663a6e5864db020ff04767ade
Christina Jochim <christina.jochim@krones.com>
2020-04-20 14:15:55 +0200

    IoCConfiguration, Autofac, Custom Markup Extension, App Configuration

---
+++ MarkupExtensions/ViewModelBindingExtension.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace TodoApp.MVVM.MarkupExtensions
+{
+    //eigene Markup-Extension für das ViewModel: braucht man, wenn man mehrere ViewModels hat und zwischen den switchen möchte
+    class ViewModelBindingExtension : MarkupExtension
+    {
+        public Type ViewModelType { get; set; }
+
+        //ProvideValue: Wenn in einer abgeleiteten Klasse implementiert, wird ein Objekt zurückgegeben, 
+        //das als Wert der Zieleigenschaft für diese Markuperweiterung bereitgestellt wird.
+        public override object ProvideValue(IServiceProvider _) // _-Operator/Discard-Operator: wird nicht verwendet
+        {
+            //soll Objekt vom Typ MainWindowViewModel zurückgeben 
+            var viewModel = IoCConfiguration.Resolve(ViewModelType); 
+            return viewModel;
+        }
+    }
+}
---
+++ Services/IAppConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoApp.MVVM.Services
+{
+    interface IAppConfiguration
+    {
+        string TodoItemFilePath { get; } //property für die TodoItem Datei
+
+        string TagFilePath { get; }
+    }
+}
